13ad6665ddb989908077f699527a9637089ce05e
browissimo <browissimo@gmail.com>
2022-08-28 17:03:12 +0300

    Добавьте файлы проекта.

---
+++ TelegraphComicsUploader/FilesGetter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TelegraphComicsUploader
+{
+    internal class FilesGetter
+    {
+        private readonly string? folderPath = String.Empty;
+        public FilesGetter(string? folderPath)
+        {
+            this.folderPath = folderPath;
+        }
+
+        public List<string> GetFilesPaths()
+        {
+            if (Directory.Exists(folderPath))
+            {
+                return Directory.GetFiles(folderPath).ToList<string>();
+            }
+            else
+            {
+                Console.WriteLine("Папки не существует");
+                return new List<string> { };
+            }
+        }
+
+        public string GetFolderName()
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(folderPath);
+                return dir.Name;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ TelegraphComicsUploader/Program.cs
@@ -0,0 +1,34 @@
+﻿using telegraph;
+using telegraph.Node;
+using TelegraphComicsUploader;
+
+class Program
+{
+    public static string token = "b39105bee4a36ec7878dd41f0e13e846ed87e5f2381347d51d4cab33eb98";
+    static async Task Main(string[] args)
+    {
+
+        Console.WriteLine("Путь к папке с комиксом");
+        var comixPath = Console.ReadLine();
+
+        FilesGetter filesGetter = new FilesGetter(comixPath);
+        var imagerToUpload = filesGetter.GetFilesPaths();
+
+        Console.WriteLine($"Количество страниц {imagerToUpload.Count}");
+
+        NodeElement node = new NodeElement();
+
+        node.AddText("Больше топовых комиксов в Новая папка (2)");
+        node.AddLink("https://t.me/artporncom");
+
+        var imgs = await ApiList.Upload(imagerToUpload);
+        foreach (var img in imgs.Srcs)
+        {
+            node.AddImage(img.Src);
+        }
+
+        var resultPage = await ApiList.CreatePage(token, $"{filesGetter.GetFolderName()}", node.ToJson());
+        Console.WriteLine(resultPage.Url.AbsoluteUri);
+        Console.ReadLine();
+    }
+}
